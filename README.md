70d8f2a72e42dd30816aeb440da325674f81f879
deborasous <deborapatricia27@gmail.com>
2022-04-07 22:03:53 -0300

    Acrescentando condições
    Criando condições para alteração de bg e imagem.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hora atual
 
 Altera imagem conforme hora atual do sitema operacional.
+
+nova alteração
--- execHora.js
+++ execHora.js
@@ -1,11 +1,19 @@
 function carregar() {
     var msg = document.querySelector(".msg")
     var img = document.querySelector(".image")
+  
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12) {
-        img.setAttribute = 'image/manha.png'   
-        }    
+    if (hora >= 0 && hora < 12){
+        img.src = 'image/manha.png'
+        document.body.style.background = '#809da5'
+    }else if (hora >= 12 && hora < 18){
+        img.src = 'image/tarde.jpg'
+        document.body.style.background = '#ee8301'
+    }else{
+        img.src ='image/noite.png'
+        document.body.style.background = '#59456d'
+    }
 }
